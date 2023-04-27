5832e398c00e2f71e5ae6a2b25af2b52e74dcb9c
C12 <mcpartlandpatrick@gmail.com>
2023-04-27 19:27:37 -0400

    removed practice files

--- assets/script.js
+++ assets/script.js
@@ -3,6 +3,7 @@ var btn = document.getElementById('btn');
 var todayWeatherCard = document.getElementById('todayWeatherCard');
 var todayCurrentWeatherEl = document.getElementById('currentWeather');
 var todayCurrentTempEl = document.getElementById('currentTemp');
+var forecastContainer = document.getElementById('forecast');
 
 
 var apiKey = 'cf3b5ece75c800ed156bdbf554dc98a5'
@@ -12,69 +13,59 @@ var todayCurrentWeather;
 
 
 
-
-
-
-
-
-
-
-
-function selectLocation(){
-    var city = inputEl.value;
-    var geoUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' +city+ '&limit=5&appid=cf3b5ece75c800ed156bdbf554dc98a5'
-    fetch(geoUrl)
-    .then(function(response){
+ function selectLocation(){
+     var city = inputEl.value;
+   var geoUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' +city+ '&limit=5&appid=cf3b5ece75c800ed156bdbf554dc98a5'
+     fetch(geoUrl)
+     .then(function(response){
        return response.json();
-    })
+     })
     .then(function(data){
         var latitude = data[0].lat;
-        var longitude = data[0].lon;
-        var weatherUrl = 'http://api.openweathermap.org/data/2.5/forecast?lat='+latitude+'&lon='+longitude+'&appid='+apiKey+'&units=imperial';
+         var longitude = data[0].lon;
+         var weatherUrl = 'http://api.openweathermap.org/data/2.5/forecast?lat='+latitude+'&lon='+longitude+'&appid='+apiKey+'&units=imperial';
 
 
-        console.log('Latitude for ' + city +':',latitude);
-        console.log('Longitude for ' + city + ':',longitude);
-        fetch(weatherUrl)
-        .then(function(response){
-            return response.json();
-        })
-        .then(function(data){
-            //console.log(data);
-            console.log(data);
-            //Creating short hand variables for collected data for current day
-            var todayCurrentDescription = data.list[0].weather[0].description;
-            var todayCurrentIcon = data.list[0].weather[0].icon;
-            var todayCurrentWeather = data.list[0].weather[0].main;
-            var todayCurrentTemp = data.list[0].main.temp;
-            var todayDate;
-            //Vars for following days data
-            var dayOneDescription = data.list[7].weather[0].description;
-            var dayOneIcon = data.list[7].weather[0].icon;
-            var dayOneWeather = data.list[7].weather.main;
-            var dayOneTemp = data.list[7].weather.temp;
-            var dayOneDate;
+         console.log('Latitude for ' + city +':',latitude);
+         console.log('Longitude for ' + city + ':',longitude);
+         fetch(weatherUrl)
+         .then(function(response){
+             return response.json();
+         })
+         .then(function(data){
+             //console.log(data);
+             console.log(data);
+             //Creating short hand variables for collected data for current day
+             var todayCurrentDescription = data.list[0].weather[0].description;
+             var todayCurrentIcon = data.list[0].weather[0].icon;
+             var todayCurrentWeather = data.list[0].weather[0].main;
+             var todayCurrentTemp = data.list[0].main.temp;
+             var todayDate;
+             //Vars for following days data
+             var dayOneDescription = data.list[7].weather[0].description;
+             var dayOneIcon = data.list[7].weather[0].icon;
+             var dayOneWeather = data.list[7].weather.main;
+             var dayOneTemp = data.list[7].weather.temp;
+             var dayOneDate;
 
 
-            console.log('Today current temp: ',todayCurrentTemp);
-            console.log('Today current weather: ',todayCurrentWeather);
-            console.log('Today current weather description: ',todayCurrentDescription);
-            
-            todayWeatherCard.classList.remove('hide');
-            todayCurrentWeatherEl.textContent=todayCurrentWeather
-            todayCurrentTempEl.textContent=todayCurrentTemp + ' degrees F'
-                      
-            
-            
-        })
-    }) 
-};
+             console.log('Today current temp: ',todayCurrentTemp);
+             console.log('Today current weather: ',todayCurrentWeather);
+             console.log('Today current weather description: ',todayCurrentDescription);
+
+             todayWeatherCard.classList.remove('hide');
+             todayCurrentWeatherEl.textContent=todayCurrentWeather
+             todayCurrentTempEl.textContent=todayCurrentTemp + ' degrees F'
 
 
 
+         })
+     }) 
+ };
 
 
-btn.addEventListener('click', function(event){
-    event.preventDefault();
-    selectLocation();
-})
+
+
+
+
+btn.addEventListener('click', selectLocation)
